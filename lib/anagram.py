fbb3acfab6538f5cd39c06f7474709bb1df40d07
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-05-27 20:25:18 +0300

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, list):
+        matches = []
+        for candidate in list:
+            if self.anagram(candidate): # check if the candidate word is an anagram of self.word
+                matches.append(candidate)
+        return matches
+    
+    def anagram(self, candidate):
+        return sorted(self.word.lower()) == sorted(candidate.lower()) # check if sorted letters of self.word and the candidate word are equal
